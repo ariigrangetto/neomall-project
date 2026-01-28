d5444973141bf4ee71b2cc3e6265fc654b7eff4c
Arianna <ariigrangetto5@gmail.com>
2026-01-28 17:02:23 -0300

    feat: mySql connection

---
+++ server/config/mySql/mySqlConnection.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+import "dotenv/config";
+
+export const createDBConnection = async () => {
+  return mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+};
