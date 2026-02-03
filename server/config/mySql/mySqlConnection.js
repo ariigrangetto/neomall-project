bdc20578f1a97ec7ba4f35449561f9bb0ed50ccd
Arianna <ariigrangetto5@gmail.com>
2026-02-03 10:16:38 -0300

    feat: add pool mysql connection

--- server/config/mySql/mySqlConnection.js
+++ server/config/mySql/mySqlConnection.js
@@ -1,12 +1,16 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
+//create pool -> reduce the time spent connecting to the mySql server by reusing a previous connection;
+
 export const createDBConnection = async () => {
-  return mysql.createConnection({
+  const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
   });
+  const connection = await pool.getConnection();
+  return connection;
 };
