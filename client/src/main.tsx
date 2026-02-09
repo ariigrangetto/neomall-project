2ef11dfab092aa756984281e5e62440ee69610bc
Arianna <ariigrangetto5@gmail.com>
2026-02-09 12:17:28 -0300

    feat: delete pagination provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,15 +4,12 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { FilterProvider } from "./context/FilterContext.tsx";
-import { PaginationProvider } from "./context/PaginationContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
       <FilterProvider>
-        <PaginationProvider>
-          <App />
-        </PaginationProvider>
+        <App />
       </FilterProvider>
     </AuthProvider>
   </BrowserRouter>,
